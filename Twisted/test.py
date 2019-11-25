ebd8aea970261f34b07ba9a2f984886bcd4dc81c
Victor <Vict2904@stud.kea.dk>
2019-11-25 20:30:45 +0100

    added REGEX til mail check, i XPATH

---
+++ Twisted/test.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+sys.path.append('C:/Users/Tarkuen/Python Projects/Hovedopgave/MVCHovedopgave/scrapy1/prot_82_scrapy')
+
+import prot_82_scrapy
+
+os.system('cd ../scrapy1/prot_82_scrapy && scrapy crawl spider1')
