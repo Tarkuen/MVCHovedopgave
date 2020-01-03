204bcad584a7e2b5f6c624cadb4cc5c1fe33191f
Victor <Vict2904@stud.kea.dk>
2020-01-03 13:46:19 +0100

    syntax changes

---
+++ scrapy_mod/prot_82_scrapy/prot_82_scrapy/spiders/rootSpider.py
@@ -0,0 +1,39 @@
+from os import path
+import collections, json, re
+
+import scrapy as Scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+from prot_82_scrapy.items import Email_Item
+
+
+class RootSpider(CrawlSpider):
+    name = 'spider2'
+    rules=[
+            Rule(LinkExtractor(allow=(r'/*')), callback='parse_item', follow=True),
+        ]
+
+    def __init__(self, url='', *args, **kwargs):
+        super(RootSpider, self).__init__(*args, **kwargs)
+        
+        domain=str(url).split('//')[1].split('/')[0]
+        self.allowed_domains = [domain,]
+        self.start_urls = [str(url)+'/',]
+        self.filename= 'output.json'
+        self.xpath_target="//a[contains(@href,'@')]"
+
+    def parse_item(self, response):
+        select = Scrapy.Selector(response=response)
+        
+        if len(select.xpath(self.xpath_target).getall()) == 0:
+            item=Email_Item(emailAddress="no emails",emailPage=f"{page}")
+            return item
+        self.xpath_target="//a[contains(@href,'@')]/ancestor::div/child::a | //a[contains(@href,'@')]/ancestor::div/child::p "
+
+        for link in select.xpath(self.xpath_target).getall():
+            link= re.sub(r'(?:style\=)(?:.*)(?:\;\")' , "", link)
+            item=Email_Item(emailAddress=str(link).strip('mailto:'),emailPage=f"{response.url.split('//')[1]}")
+            yield item
+
+        self.log('emails appended to %s' % self.filename)
---
+++ scrapy_mod/prot_82_scrapy/prot_82_scrapy/spiders/targetSpider.py
@@ -0,0 +1,33 @@
+from os import path
+
+import scrapy as Scrapy
+import collections, json, re
+
+from prot_82_scrapy.items import Email_Item
+
+class TargetSpider(Scrapy.Spider):
+    name = 'spider1'
+
+    def __init__(self, url='', *args, **kwargs):
+        super(TargetSpider, self).__init__(*args, **kwargs)
+        self.cURL = [url]  # py36
+        self.filename= 'output.json'
+        self.xpath_target="//a[contains(@href,'@')]"
+    def start_requests(self):
+        
+        for url in self.cURL:
+            yield Scrapy.Request(url=url, callback=self.parse)
+    
+    def parse(self, response):
+        select = Scrapy.Selector(response=response)
+        if len(select.xpath(self.xpath_target).getall()) == 0:
+            item=Email_Item(emailAddress="no emails",emailPage=f"{response.url.split('//')[1]}")
+            return item
+
+        xpath_target="//a[contains(@href,'@')]/ancestor::div/child::a | //a[contains(@href,'@')]/ancestor::div/child::p "
+        for link in select.xpath(xpath_target).getall():
+            link= re.sub(r'(?:style\=)(?:.*)(?:\;\")' , "", link)
+            item=Email_Item(emailAddress=str(link).strip('mailto:'),emailPage=f"{page}")
+            yield item
+
+        self.log('emails appended to %s' % self.filename)
