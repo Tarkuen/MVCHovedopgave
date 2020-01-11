9591ce411d64de1c47088578551b31d1a132e1da
Victor <Vict2904@stud.kea.dk>
2020-01-11 11:38:52 +0100

    TargetSpider documentation added

--- Scrapy/scrapy_project/scrapy_project/spiders/targetSpider.py
+++ Scrapy/scrapy_project/scrapy_project/spiders/targetSpider.py
@@ -6,6 +6,30 @@ import collections, json, re
 from scrapy_project.items import Email_Item
 
 class TargetSpider(Scrapy.Spider):
+    """
+    Parameters:
+    -----------
+        Scrapy.Spider : Spider 
+            Object passed to the function by the active Scrapy crawler.
+
+        Attributes:
+        -----------
+        Name : String 
+            Name of spider for command line execution
+
+    Methods:
+    -----------
+
+        __init__
+            lorem ipsum
+
+        start_requests
+            lorem ipsum
+
+        parse
+            lorem ipsum
+
+    """
     name = 'spider1'
 
     def __init__(self, url='', *args, **kwargs):
@@ -19,6 +43,42 @@ class TargetSpider(Scrapy.Spider):
             yield Scrapy.Request(url=url, callback=self.parse)
     
     def parse(self, response):
+        """
+        Overridden method from Spider super class. 
+        Handles the results yielded by Scrapy.Downloader component.
+        
+        Parameters:
+        -----------
+        response : HTMLResponse 
+            Object passed to the function by the active Scrapy crawler.
+
+        Attributes:
+        -----------
+        @select : Scrapy.Selector
+            Scrapy API to different HTML Selector libraries.
+            This object uses the XPath implementation
+        
+        @item : Email_Item
+            Item stores the result of our XPath search and it uses Scrapys own Item interface.
+            It is created during the for loop over found HTML elements.
+
+        @xpath_target : str
+            Regular expression used to select relevant HTML elements with the 'select' attribute.
+            Currently chooses the found e-mail address' ancestors div element or near paragraph item
+
+        Returns:
+        ----------
+        Yields items :
+        Items are picked up and stored in Scrapys Middleware object.
+        These items are then stored in the 'output.json' file in the root directory.
+
+        Scrapy Contract Test
+        ----------
+        
+            @url https://www.dr.dk/presse/kontakt
+            @scrapes emailAddress emailPage
+
+        """
         select = Scrapy.Selector(response=response)
         if len(select.xpath(self.xpath_target).getall()) == 0:
             item=Email_Item(emailAddress="no emails",emailPage=f"{response.url.split('//')[1]}")
